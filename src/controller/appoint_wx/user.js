01b6bed832ef9eafeb2fe0a3899fd06534f4dae8
root <447818666@qq.com>
2019-11-05 12:05:32 +0800

    案例管理员增删改

--- src/controller/appoint_wx/user.js
+++ src/controller/appoint_wx/user.js
@@ -4,12 +4,277 @@ const request = require('request');
 const Response = require('../../config/response')
 const Util = require('../../util/Util')
 const DateUtil = require('../../util/DateUtil')
-const logger =think.logger;
+const md5 = require('md5')
+const logger = think.logger;
 
 
 module.exports = class extends Base {
 
 
+    /**
+     * 新增用户
+     * @returns {Promise<boolean>}
+     */
+    async addAction() {
+        try {
+
+            let name = this.post('name')
+            let phone = this.post('phone')
+            let gender = this.post('gender')
+            let birthday = this.post('birthday')
+            let email = this.post('email')
+            let role = this.post('role')
+
+            logger.info(`新增用户参数 name:${name}， phone:${phone}， gender:${gender}， birthday:${birthday}， email:${email}， role:${role}`)
+
+            if (!name) {
+                this.body = Response.businessException(`姓名不能为空！`)
+                return false;
+            }
+
+            if (!phone) {
+                this.body = Response.businessException(`手机号不能为空！`)
+                return false;
+            }
+
+            if (!gender) {
+                this.body = Response.businessException(`性别不能为空！`)
+                return false;
+            }
+
+            if (!birthday) {
+                this.body = Response.businessException(`出生日期不能为空！`)
+                return false;
+            }
+
+            if (!email) {
+                this.body = Response.businessException(`电子邮件不能为空！`)
+                return false;
+            }
+
+            if (!role) {
+                this.body = Response.businessException(`用户类型不能为空！`)
+                return false;
+            }
+
+            let op_date=DateUtil.getNowStr()
+
+
+
+            let addJson={
+                name,
+                phone,
+                gender,
+                birthday,
+                email,
+                op_date,
+                role
+            }
+
+            if(role!==0){
+                let password=md5('123456')
+                addJson.password=password
+            }
+
+            let data = await this.model('user').add(addJson);
+
+            logger.info(`新增用户，数据库返回：${JSON.stringify(data)}`)
+
+            this.body = Response.success(data);
+
+        } catch (e) {
+            logger.info(`新增用户异常 msg:${e}`);
+            this.body = Response.businessException(e);
+        }
+
+
+    }
+
+    /**
+     * 删除用户
+     * @returns {Promise<boolean>}
+     */
+    async deleteAction() {
+        try {
+
+            let id = this.post('id')
+
+            logger.info(`删除用户参数 id:${id}`)
+
+            if (!id) {
+                this.body = Response.businessException(`用户ID不能为空！`)
+                return false;
+            }
+
+
+            let data = await this.model('user').where({
+                id,
+            }).delete()
+
+            logger.info(`删除用户，数据库返回：${JSON.stringify(data)}`)
+
+            this.body = Response.success(data);
+
+        } catch (e) {
+            logger.info(`删除用户异常 msg:${e}`);
+            this.body = Response.businessException(e);
+        }
+
+
+    }
+
+    /**
+     * 修改用户信息
+     * @returns {Promise<boolean>}
+     */
+    async updateAction() {
+        try {
+
+            let id = this.post('id')
+            let name = this.post('name')
+            let phone = this.post('phone')
+            let gender = this.post('gender')
+            let birthday = this.post('birthday')
+            let email = this.post('email')
+
+            logger.info(`修改用户信息参数 id:${id}，name:${name}， phone:${phone}， gender:${gender}， birthday:${birthday}， email:${email}`)
+
+            let updateJson={}
+            if (name) {
+                updateJson.name=name
+            }
+
+            if (phone) {
+                updateJson.phone=phone
+            }
+
+            if (gender) {
+                updateJson.gender=gender
+            }
+
+            if (birthday) {
+                updateJson.birthday=birthday
+            }
+
+            if (email) {
+                updateJson.email=email
+            }
+
+            let op_date=DateUtil.getNowStr()
+
+            updateJson.op_date=op_date;
+
+            let data = await this.model('user').where({
+                id
+            }).update(updateJson);
+
+            logger.info(`修改用户信息，数据库返回：${JSON.stringify(data)}`)
+
+            this.body = Response.success(data);
+
+        } catch (e) {
+            logger.info(`修改用户信息异常 msg:${e}`);
+            this.body = Response.businessException(e);
+        }
+
+
+    }
+
+    /**
+     * 获取用户列表
+     * @returns {Promise<boolean>}
+     */
+    async listAction() {
+        try {
+
+            let role = this.post('role')
+            let page = this.post('page')||1
+            let pageSize = this.post('pageSize')||2
+
+            logger.info(`获取用户列表参数 role:${role}`)
+
+            if (!role) {
+                this.body = Response.businessException(`用户类型不能为空！`)
+                return false;
+            }
+
+
+            let data = await this.model('user').where({
+                role
+            }).page(page,pageSize).countSelect();
+
+            logger.info(`获取用户列表，数据库返回：${JSON.stringify(data)}`)
+
+            this.body = Response.success(data);
+
+        } catch (e) {
+            logger.info(`获取用户列表异常 msg:${e}`);
+            this.body = Response.businessException(e);
+        }
+
+
+    }
+
+
+    /**
+     * pc端用户登录
+     * @returns {Promise<void>}
+     */
+    async loginAction() {
+        try {
+
+            let phone = this.post('phone')
+            let password = this.post('password')
+
+            logger.info(`pc端用户登录参数 phone:${phone}， password:${password}`)
+
+            if (!phone) {
+                this.body = Response.businessException(`手机号不能为空！`)
+                return false;
+            }
+
+            if (!password) {
+                this.body = Response.businessException(`密码不能为空！`)
+                return false;
+            }
+
+            password=md5(password)
+
+
+            let data = await this.model('user').where({
+                phone
+            }).find();
+
+            logger.info(`pc端用户登录根据手机号查询，数据库返回：${JSON.stringify(data)}`)
+
+            if (Util.isEmptyObject(data)) {
+                this.body = Response.businessException(`手机号不存在！`);
+            } else {
+
+                data = await this.model('user').where({
+                    phone,
+                    password
+                }).find();
+
+                logger.info(`pc端用户登录根据手机号和密码查询，数据库返回：${JSON.stringify(data)}`)
+
+                if (Util.isEmptyObject(data)) {
+                    logger.info(Response.businessException(`密码不正确！`))
+                    logger.info(Response.systemException(`密码不正确！`))
+                    logger.info(Response.success(`成功！`))
+                    this.body = Response.businessException(`密码不正确！`);
+                } else {
+                    this.body = Response.success(data);
+                }
+            }
+
+        } catch (e) {
+            logger.info(`pc端用户登录异常 msg:${e}`);
+            this.body = Response.businessException(e);
+        }
+
+
+    }
 
     /**
      * 根据openid获取对应的c端用户信息
@@ -18,11 +283,11 @@ module.exports = class extends Base {
      */
     async getUserByOpenidAction() {
 
-        let openid =this.get('openid')
+        let openid = this.get('openid')
 
         logger.info(`根据openid获取对应的c端用户信息参数 openid:${openid}`);
 
-        try{
+        try {
 
             let data = await this.model('weixin_user').where({
                 openid
@@ -30,13 +295,13 @@ module.exports = class extends Base {
 
             logger.info(`根据openid查询user_id数据库返回：${JSON.stringify(data)}`)
 
-            if(Util.isEmptyObject(data)){
+            if (Util.isEmptyObject(data)) {
                 this.body = Response.success();
-            }else{
-                const user_id=data.user_id;
+            } else {
+                const user_id = data.user_id;
 
                 data = await this.model('user').where({
-                    id:user_id
+                    id: user_id
                 }).find();
 
                 logger.info(`根据user_id查询用户信息数据库返回：${JSON.stringify(data)}`)
@@ -44,7 +309,7 @@ module.exports = class extends Base {
                 this.body = Response.success(data);
             }
 
-        }catch (e) {
+        } catch (e) {
             logger.info(`根据openid获取对应的c端用户信息异常 msg:${e}`);
             this.body = Response.businessException(e);
         }
@@ -58,49 +323,49 @@ module.exports = class extends Base {
      */
     async registerAndBindAction() {
 
-        let openid              =this.post('openid'),
-            phone               =this.post('phone'),
-            identification_no   =this.post('identification_no'),
-            gender              =this.post('gender'),
-            email               =this.post('email'),
-            name                =this.post('name'),
-            birthday            =this.post('birthday');
+        let openid = this.post('openid'),
+            phone = this.post('phone'),
+            identification_no = this.post('identification_no'),
+            gender = this.post('gender'),
+            email = this.post('email'),
+            name = this.post('name'),
+            birthday = this.post('birthday');
 
         logger.info(`用户注册，同时将userId和openid绑定参数 openid:${openid}, phone:${phone}, identification_no:${identification_no}, gender:${gender}, email:${email}, birthday:${birthday},name:${name},`);
 
-        if(!openid){
-            this.body=Response.businessException(`openid不能为空！`)
+        if (!openid) {
+            this.body = Response.businessException(`openid不能为空！`)
             return false;
         }
 
-        if(!phone){
-            this.body=Response.businessException(`手机号不能为空！`)
+        if (!phone) {
+            this.body = Response.businessException(`手机号不能为空！`)
             return false;
         }
 
-        if(!identification_no){
-            this.body=Response.businessException(`身份证号不能为空！`)
+        if (!identification_no) {
+            this.body = Response.businessException(`身份证号不能为空！`)
             return false;
         }
 
-        if(!gender){
-            this.body=Response.businessException(`性别不能为空！`)
+        if (!gender) {
+            this.body = Response.businessException(`性别不能为空！`)
             return false;
         }
 
-        if(!email){
-            this.body=Response.businessException(`电子邮件不能为空！`)
+        if (!email) {
+            this.body = Response.businessException(`电子邮件不能为空！`)
             return false;
         }
 
-        if(!birthday){
-            this.body=Response.businessException(`出生日期不能为空！`)
+        if (!birthday) {
+            this.body = Response.businessException(`出生日期不能为空！`)
             return false;
         }
 
-        try{
+        try {
 
-            let op_date=DateUtil.getNowStr()
+            let op_date = DateUtil.getNowStr()
 
             let user_id = await this.model('user').add({
                 openid,
@@ -108,7 +373,7 @@ module.exports = class extends Base {
                 identification_no,
                 gender,
                 email,
-                birthday:DateUtil.format(birthday,'date'),
+                birthday: DateUtil.format(birthday, 'date'),
                 name,
                 op_date
             })
@@ -124,7 +389,7 @@ module.exports = class extends Base {
 
             this.body = Response.success(user_id);
 
-        }catch (e) {
+        } catch (e) {
             logger.info(`用户注册，同时将userId和openid绑定异常 msg:${e}`);
             this.body = Response.businessException(e);
         }
@@ -133,10 +398,9 @@ module.exports = class extends Base {
     }
 
 
-
     async indexAction() {
 
-        let token= await this.getToken();
+        let token = await this.getToken();
 
         this.json({token: token})
 
@@ -147,7 +411,7 @@ module.exports = class extends Base {
 
         return new Promise(((resolve, reject) => {
             request.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx983e6f0f3345a6c6&secret=bccf7d2d46b446ec5749fc173f1e051b`, (error, response, body) => {
-                console.log(3333333,error)
+                console.log(3333333, error)
 
                 if (error) {
                     resolve(error)
