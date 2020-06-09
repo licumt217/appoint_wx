bb847d6fe914c9eb1fd3dbdbbaaa782e20a14910
root <447818666@qq.com>
2020-06-09 19:52:49 +0800

    预约后支付，定时任务执行，订单结束后重新生成一个新的单子

--- src/schedule/index.js
+++ src/schedule/index.js
@@ -21,7 +21,7 @@ const  scheduleCronstyle = async ()=>{
 
 /**
  * 每小时执行任务，订单：
- * 1、已审核（待支付）：预约前支付，超过预约开始时间，状态改为已取消。
+ * 1、已审核（待支付）：预约前支付，超过预约开始时间，状态改为已取消；预约后支付，订单结束后下一天，如果是连续预约，则自动生成下一条订单。
  * 2、已支付：预约前支付，超过预约结束时间24小时，状态改为已完结。
  */
 const scheduleOrder=async ()=>{
@@ -100,7 +100,7 @@ const handlePayedOrders=async ()=>{
     }
 }
 /**
- * 1、已审核（待支付）：预约前支付，超过预约开始时间，状态改为已取消。
+ * 1、已审核（待支付）：预约前支付，超过预约开始时间，状态改为已取消；预约后支付，订单结束后下一天，如果是连续预约，则自动生成下一条订单。
  * @returns {Promise<void>}
  */
 const handleAuditedOrders=async ()=>{
@@ -127,6 +127,15 @@ const handleAuditedOrders=async ()=>{
                     await appointmentService.cancel(order.appointment_id)
                 }
 
+            }else if(order.fee_type===FEE_TYPE.AFTER_MONTH || order.fee_type===FEE_TYPE.AFTER_SINGLE){
+                let order_date=new Date(order.order_date);
+
+                let now_date=new Date();
+                now_date.setDate(now_date.getDate()-1)
+                //未精确到具体时段。暂时以天计算。生成一个新的订单
+                if(DateUtil.before(order_date,now_date)){
+                    await orderService.add(order.appointment_id)
+                }
             }
         }
     }
